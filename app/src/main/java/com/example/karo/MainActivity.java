482776e5616af4e8f4fa839b7e388eae9f69a776
Nabcellent <nabcellent.dev@gmail.com>
2022-09-10 18:06:49 +0300

    Implements Navigation Drawer
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- app/src/main/java/com/example/karo/MainActivity.java
+++ app/src/main/java/com/example/karo/MainActivity.java
@@ -20,9 +20,7 @@ public class MainActivity extends AppCompatActivity {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.navigation_view);
 
-        toolbar.setNavigationOnClickListener(view -> {
-            drawer.openDrawer(GravityCompat.START);
-        });
+        toolbar.setNavigationOnClickListener(view -> drawer.openDrawer(GravityCompat.START));
 
         navigationView.setNavigationItemSelectedListener(item -> {
             drawer.closeDrawer(GravityCompat.START);
@@ -31,20 +29,23 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.home:
                     Helpers.showToast(this, "Home is Clicked");
                     break;
-                case R.id.trash:
-                    Helpers.showToast(this, "Trash is Clicked");
+                case R.id.manage_fees:
+                    Helpers.showToast(this, "Manage Fees is Clicked");
                     break;
                 case R.id.settings:
                     Helpers.showToast(this, "Settings is Clicked");
                     break;
-                case R.id.login:
-                    Helpers.showToast(this, "Login is Clicked");
+                case R.id.profile:
+                    Helpers.showToast(this, "Profile is Clicked");
                     break;
                 case R.id.share:
                     Helpers.showToast(this, "Share is Clicked");
                     break;
                 case R.id.rate:
                     Helpers.showToast(this, "Rate is Clicked");
+                    break;
+                case R.id.logout:
+                    Helpers.showToast(this, "Sign Out is Clicked");
                     break;
                 default:
                     return true;
